6ff0158dc696ccd6b410774672788a1cf8b99892
bobser <144664653+bobser223@users.noreply.github.com>
2024-01-22 20:58:40 +0200

    amend fourth commit

--- main.kt
+++ main.kt
@@ -9,7 +9,7 @@ for (int i = 0; i < 10; i++) {
 // далі пітон, бо вивчати нову мову програмування не в моїх планах на цю ніч (P.S. знаю що дефки спочатку, але тут 2 різні мови)
 
 def vivaMechMat():
-    print("Viva Mech-mat faculty")
+    print("Viva Mech-mat faculty!!!")
 
 
 
