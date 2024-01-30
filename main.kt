b5797fc62ddc7f49b3614ab41f9fbecc1c66593b
bobser <144664653+bobser223@users.noreply.github.com>
2024-01-30 09:10:32 +0200

    bob commit

--- main.kt
+++ main.kt
@@ -2,7 +2,7 @@ for (int i = 0; i < 10; i++) {
     printf("hello world\n");
 }
 
-
+bcbbhcbfd
 
 
 
@@ -15,3 +15,59 @@ def vivaMechMat():
 
 
 vivaMechMat()
+
+// далі моя шиза на плюсах
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+bool isPrime(int a) {
+    if (a < 2) {
+        return false;
+    }
+
+    for (int i = 2; i <= static_cast<int>(sqrt(a)); ++i) {
+        if (a % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::vector<int> r(int a) {
+    std::vector<int> primes;
+
+    if (a == 1) {
+        return primes;
+    }
+
+    if (isPrime(a)) {
+        primes.push_back(a);
+        return primes;
+    } else {
+        for (int i = 2; i <= a; ++i) {
+            if (a % i == 0) {
+                primes.push_back(i);
+                auto result = r(a / i);
+                primes.insert(primes.end(), result.begin(), result.end());
+                break;
+            }
+        }
+    }
+
+    return primes;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> result = r(n);
+
+    for (const auto &prime : result) {
+        std::cout << prime << " ";
+    }
+
+    return 0;
+}
